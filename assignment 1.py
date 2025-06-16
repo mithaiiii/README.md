eb6c5a101ab44372124c448668eb6fc5d19b0086
mithaiiii <harshitaaashekhawat2009@gmail.com>
2025-06-16 23:51:20 +0530

    Add files via upload

---
+++ assignment 1.py
@@ -0,0 +1,24 @@
+num1 = int(input("enter the first number: "))
+num2 = int(input("enter the second number: "))
+
+add = num1+num2
+sub = num1-num2
+multiply = num1*num2
+division = num1/num2
+
+print("add:" , add)
+print("sub:" , sub)
+print("multiply:" , multiply)
+print("divide:" , division)
+
+
+
+
+
+
+
+#task 2#
+x = input("enter your first name: ")
+y = input("enter your last name: ")
+z= x+ " " +y
+print("hello", z +  "!welcome to my python program!!")
